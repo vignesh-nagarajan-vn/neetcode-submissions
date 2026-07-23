ef920033019752296a851f32cce04f7fa37c8e63
Vignesh Nagarajan <nrvignesh08@gmail.com>
2026-07-23 16:06:15 -0700

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.py
@@ -0,0 +1,11 @@
+nums = []
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        validation = []
+        for i in nums:
+            if i in validation: return True
+            validation.append(i)
+        return False
+
+solution = Solution()
